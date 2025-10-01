40fb5d0b317b4adbd4c856d916e6767c4225fb29
JazSantanaCTC <marsantanacuzcano@crackthecode.la>
2025-10-01 11:10:25 -0500

    Agregar contenido en registrate

--- u3/repaso/src/App.jsx
+++ u3/repaso/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/footer'
 import Header from './components/header'
 import Section from './components/section'
@@ -8,9 +9,27 @@ import './index.css'
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Header></Header>
+    
+
+    <Routes>
+
+      <Route
+      path='/'
+      element={
+        <>
+        </>
+      }
+      />
+    </Routes> 
+
+
+
+
     <Section></Section>
     <Footer></Footer>
+    </BrowserRouter>
     </>
   )
 }
--- u3/repaso/src/components/section.jsx
+++ u3/repaso/src/components/section.jsx
@@ -1,5 +1,5 @@
-import "./section.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./section.css"
 function Section (){
     return (
     <>
